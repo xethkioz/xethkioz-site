896b0a2eccb8b57dd7aa2a5ffdd953caf144cbec
xethkioz <xethkioz@gmail.com>
2026-07-23 12:23:06 -0300

    fix(404): support Vercel function TypeScript target

--- api/not-found.ts
+++ api/not-found.ts
@@ -1,10 +1,10 @@
 function escapeHtml(value: string) {
   return value
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#39;')
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
 }
 
 export default function handler(request: any, response: any) {
