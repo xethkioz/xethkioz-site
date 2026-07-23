8a81ff1a636a995ba2c204ead6e4fbe23776247c
xethkioz <xethkioz@gmail.com>
2026-07-23 14:10:18 -0300

    perf(css): load Home and Green Node styles by route

---
+++ src/components/RouteCssLoader.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+let homeStyles: Promise<void> | null = null
+let greenNodeStyles: Promise<void> | null = null
+
+export function loadRouteStyles(pathname: string): Promise<void> {
+  if (pathname === '/') {
+    homeStyles ??= import('../home-portal-rings.css').then(() => undefined)
+    return homeStyles
+  }
+
+  if (pathname === '/green-node' || pathname.startsWith('/green-node/')) {
+    greenNodeStyles ??= import('../generated/green-node-shell.css').then(() => undefined)
+    return greenNodeStyles
+  }
+
+  return Promise.resolve()
+}
+
+export default function RouteCssLoader() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    void loadRouteStyles(pathname)
+  }, [pathname])
+
+  return null
+}
