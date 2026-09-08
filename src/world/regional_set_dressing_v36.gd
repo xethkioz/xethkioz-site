9421e9875e53d77898da20eab9774e08796f9d9a
xethkioz <xethkioz@gmail.com>
2026-09-08 19:11:26 -0300

    feat(v3.6): hand Cuenca dressing to authored south layout

---
+++ src/world/regional_set_dressing_v36.gd
@@ -0,0 +1,6 @@
+extends "res://src/world/regional_set_dressing.gd"
+
+func _build_cuenca() -> void:
+	# v3.6: Cuenca + acceso a Aldea están compuestos manualmente por
+	# golden_south_authored_layout_v36.gd. Evitamos superponer el viejo set dressing.
+	pass
