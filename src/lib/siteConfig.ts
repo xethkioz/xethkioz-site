9b6ee38db73d60b22790edf71b8ce3f342c13a0f
xethkioz <xethkioz@gmail.com>
2026-09-24 07:59:37 -0300

    release: stamp repository remediation Pass 34

--- src/lib/siteConfig.ts
+++ src/lib/siteConfig.ts
@@ -1,6 +1,6 @@
-export const SITE_VERSION = 'v11.4.10'
-export const SITE_RELEASE = 'Development Alpha Demo - Pass 33'
-export const SITE_BUILD_DATE = '2026-09-23'
+export const SITE_VERSION = 'v11.4.11'
+export const SITE_RELEASE = 'Repository Remediation - Pass 34'
+export const SITE_BUILD_DATE = '2026-09-24'
 export const SITE_DOMAIN = 'https://xethkioz.com.ar'
 
 export const DONATION_LINKS = {
